8e6545c0ec025fbb5ebd8ee7fb7562ddec8c1eec
pritom banerjee <60362397+pritom006@users.noreply.github.com>
2020-06-12 21:40:31 +0600

    Add files via upload

---
+++ Documents/2d array .cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int A[2][3]={
+                 {10,20,30},
+                 {21,22,23}
+
+                };
+
+    for(int row=0;row<2;row++)
+    {
+        for(int col=0;col<3;col++)
+        {
+            cout<<A[row][col]<<" ";
+        }
+        cout<<endl;
+    }
+    getch();
+}
